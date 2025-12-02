7aa314df8b5645350db98df12d0b9bc453c5f26c
OneMoveBRB <sh_amir2014@mail.ru>
2025-12-02 14:43:01 +0300

    Update tree.h

--- tree.h
+++ tree.h
@@ -7,7 +7,7 @@
 
 const int UNINITIALIZED = 0xBAD;
 
-enum TreeElemType_t {
+enum TreeElemType {
     TYPE_UNDEFINED,
     TYPE_OPERATION,
     TYPE_VARIABLE,
@@ -15,12 +15,27 @@ enum TreeElemType_t {
 };
 
 enum Operation_t {
-    UNDEFINED_OPERATION,
-    ADDITION,
-    SUBTRACTION,
-    MULTIPLICATION,
-    DIVISION,
-    EXPONENTIATION
+    OPERATION_UNDEF,
+    OPERATION_ADD,
+    OPERATION_SUB,
+    OPERATION_MUL,
+    OPERATION_DIV,
+    OPERATION_EXP,
+    OPERATION_SQRT,
+    OPERATION_LN,
+    OPERATION_LOG,
+    OPERATION_SIN,
+    OPERATION_COS,
+    OPERATION_TAN,
+    OPERATION_COT,
+    OPERATION_SINH,
+    OPERATION_COSH,
+    OPERATION_TANH,
+    OPERATION_COTH,
+    OPERATION_ASIN,
+    OPERATION_ACOS,
+    OPERATION_ATAN,
+    OPERATION_ACOT
 };
 
 union TreeElem_t {
@@ -30,7 +45,7 @@ union TreeElem_t {
 };
 
 struct Node_t {
-    TreeElemType_t type;
+    TreeElemType type;
     TreeElem_t data;
     Node_t* parent;
     Node_t* left;
@@ -67,12 +82,24 @@ typedef TreeErr_t (*TreeFunc)(Node_t**);
 TreeErr_t TreeInit(Tree_t** tree);
 TreeErr_t TreeDestroy(Tree_t** tree);
 
-TreeErr_t NodeInit(Node_t** node, ...);
+Node_t* NodeInit(Node_t* parent, Node_t* left, Node_t* right, TreeElemType type, ...);
 TreeErr_t NodeDestroy(Node_t** node);
+
+#define EmptyNodeInit NodeInit(NULL, NULL, NULL, TYPE_UNDEFINED, NULL)
+
+TreeErr_t NodeCopyData(Node_t* dest_node, Node_t* src_node);
+
+TreeErr_t InorderTraversal(Node_t* node, TreeFunc func);
+TreeErr_t PostorderTraversal(Node_t* node, TreeFunc func);
+
+Node_t** GetParentNodePointer(Node_t* node);
 
 TreeErr_t ReadTree(Tree_t* tree, char* file_name);
 TreeErr_t PrintLatexTree(Tree_t* tree);
-TreeErr_t TreeDifferentiation(Tree_t* tree, Tree_t* new_tree);
-TreeErr_t ConstOptimization(Node_t* node, Tree_t* tree);
+// TreeErr_t TreeDifferentiation(Tree_t* tree, Tree_t* new_tree);
+// TreeErr_t ConstOptimization(Node_t* node, Tree_t* tree);
+
+Node_t* TreeCopySubtree(Node_t* cur_node, Node_t* parent);
+TreeErr_t PrintTree(Tree_t* tree);
 
 #endif // TREE_H
